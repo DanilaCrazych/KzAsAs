6202bca50fe006e40137ec567d0a154dcef5fcb4
danilacrazych <danilacrazych@mail.ru>
2024-03-12 21:36:50 +0300

    New size

--- src/main/java/com/example/kzasas/HelloApplication.java
+++ src/main/java/com/example/kzasas/HelloApplication.java
@@ -11,8 +11,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("kzss.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1400, 900);
+        stage.setTitle("КЗСС сила ЭКЗ могила");
         stage.setScene(scene);
         stage.show();
     }
